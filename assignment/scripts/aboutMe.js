293af675d8edd30809b97569b339403b3806f029
Neil Hanson <pro.neilhanson@gmail.com>
2022-04-06 11:37:14 -0500

    testing line 18 again

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -95,12 +95,9 @@ switch(luckyNumber){
         break;
 }
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
-function adventurous(){
+function areYouAdventurous(adventurous){
     return(adventurous ? 'Adventures are great!' : 'How about we stay home?');
 }
 console.log(adventurous(true));
 console.log(adventurous(false));
 console.log(adventurous(null));
-
-
-
